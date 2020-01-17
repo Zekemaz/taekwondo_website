fddfc06b5c3e67b3a6673c8d634e711a508ac0ec
Gabriel Desseré Susini <gabrieldesseresusini@MacBook-Pro-de-Gabriel.local>
2020-01-17 16:20:18 +0100

    first commit

--- php_file/lexical.php
+++ php_file/lexical.php
@@ -39,3 +39,28 @@ print("</table>");
 ?>
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css" media="all" />
+    <link href="https://fonts.googleapis.com/css?family=Heebo:700|Montserrat:400,700|Oswald|Raleway:800|Roboto+Mono&display=swap" rel="stylesheet">
+
+</head>
+<body>
+   <p>obengozifbheio</p>
+
+
+</body>
+</html>
+
+
+
+<?php
+
+?>
+
+
